c9e2bde23c5c858480118ab1c36adf3c8b5a1872
ryanvoor <ryan.voor@gmail.com>
2015-05-11 14:17:49 -0400

    Added post editing, list view, (it all followed the tutorial) (except for view partials)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,49 @@
 class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def new
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		# if the update method fails b/c of validations then it returns false and doesn't update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit' 
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
