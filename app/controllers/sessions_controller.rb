69c751d788907edcf473ff986884118ca0a98728
ryanvoor <ryan.voor@gmail.com>
2015-05-15 09:38:14 -0400

    trying additional stuff to get mongo's .find stuff to work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,28 +3,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # no idea if this is going to work
-    # @user = User.find_by(username: params[:session][:username])
-  	# @user = User.find_by(params[:session][:username])
-    # puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #{User.find(params[:session][:username]).username}"
-  	@users = User.all
-    @users.each do |user|
-      if user.username == params[:session][:username]
-        @user = user
-      end
-    end
-    if (@user && @user.password == params[:session][:password])
+  	# render 'new'
+  	# b/c user validates uniqueness for the username field this should always work
+  	# @user = User.where(params[:session][:username]).first
+
+  	# this is what i would think based on my experience with ActiveRecord
+  	@user = User.find_by(params[:session][:username])
+
+  	# @user = User.find(params[:session][:user_id])
+
+  	# for this attempt it says that 'db' is not a recognized variable
+  	# @user = db.User.all.findOne(username: params[:session][:username])
+  	# most recent attempt based on something I saw near the end of the 194 railscasts videos
+  	# @user = User.all(:username => params[:session][:username])
+  	if @user && @user.password == params[:session][:password]
   		log_in @user
-      redirect_to user_path(@user)
+  		redirect_to user_path(@user)
   	else
-  		# This isn't completely correct according to the tutorial
-  		# flash[:danger] = 'Invalid username/password combination'
+  		# the tutorial wanted me to create an error message here
   		render 'new'
   	end
   end
 
   def destroy
-  	# should have other stuff here
   	log_out
   	redirect_to root_path
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,29 +1,10 @@
 module SessionsHelper
-	# logs in the given user
 	def log_in(user)
 		session[:user_id] = user.id
 	end
 
-	def current_user
-		# @current_user ||= User.find_by(id: session[:user_id])
-		# @current_user ||= User.where(id: session[:user_id]).first
-		@users = User.all
-		@users.each do |user|
-			if user.id == session[:user_id]
-				@current_user = user
-			end
-		end
-		@current_user
-	end
-
-	def logged_in?
-		!current_user.nil?
-	end
-
-	# logs out the current user
 	def log_out
 		session.delete(:user_id)
-		@current_user = nil
-		puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+  	@current_user = nil
 	end
 end
